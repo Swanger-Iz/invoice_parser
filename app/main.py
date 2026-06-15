d1cb0b12363b99b34070f6384ad18470aa2658c7
Ilgiz Magadeiv <swanger789@gmail.com>
2026-06-15 21:22:23 +0300

    Created FIO get endpoint

--- app/main.py
+++ app/main.py
@@ -1,31 +1,27 @@
 import io
 import sys
 import time
-import uuid
 from pathlib import Path
-from typing import Literal
 
-from langchain.agents.structured_output import StructuredOutputValidationError
-from langchain.messages import HumanMessage
-from PIL import Image
-from pydantic import BaseModel
+import uvicorn
 
 sys.path.insert(1, str(Path(__file__).parent.parent))  # Вставляю путь invoice_ai
 
+
+from fastapi import FastAPI, File, Request, UploadFile
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from langchain.agents.structured_output import StructuredOutputValidationError
+from langchain.messages import HumanMessage
+from PIL import Image
+
 from models.models import ExtractionModel
-
-
-# Caching
-class ModelRequests(BaseModel):
-    id: str
-    status: Literal["good", "bad"]
-    image_bytes: bytes
-
+from schemas.main_schemas import ModelRequests
 
 model_requests = []
 
 invoice_ai_dir = Path(__file__).parent.parent
-print("Working directory:", str(invoice_ai_dir))
+# print("Working directory:", str(invoice_ai_dir))
 
 # Init a image
 image_path = invoice_ai_dir / "data" / "orion_agreement.png"
@@ -38,18 +34,29 @@ image_bytes = byte_buffer.getvalue()
 # LLM init
 extractor_agent = ExtractionModel.init_extractor_agent()
 
+
 # llm params
-messages = [HumanMessage(content_blocks=[{"type": "text", "text": "Проанализируй договор картинки полученной в битовом формате!"}])]
-config = {"configurable": {"image_bytes": image_bytes}}
+def init_configs(image_bytes: bytes):
+    messages = [HumanMessage(content_blocks=[{"type": "text", "text": "Проанализируй договор картинки полученной в битовом формате!"}])]
+    config = {"configurable": {"image_bytes": image_bytes}}
+
+    return (messages, config)
 
 
 # llm invoke
-def safely_exec_agent(agent, attempts=3):
+def safely_exec_agent(agent, attempts=3, image_in_bytes: bytes = None):
+    if image_in_bytes is None:
+        return None
+    messages, config = init_configs(image_bytes=image_in_bytes)
+    print("Safely starting the agent")
     for attempt in range(attempts):
+        print(f"Attempt - {attempt+1}\n")
         try:
+            start_time = time.time()
             response = agent.invoke({"messages": messages}, config=config)
-            print("\t---!!Success!!---")
-            model_requests.append(ModelRequests(id=uuid.uuid2(), status="good", image_bytes=config["configurable"]["image_bytes"]))
+            print("=" * 20, "Success", "=" * 20)
+            model_requests.append(ModelRequests(id=len(model_requests) + 1, status="good", image_bytes=config["configurable"]["image_bytes"]))
+            print(f"Response time = {time.time() - start_time:.2f}")
             return response
         except StructuredOutputValidationError:
             print(f"Попытка {attempt+1}: пустой/невалидный ответ модели, повтор...")
@@ -60,14 +67,52 @@ def safely_exec_agent(agent, attempts=3):
                 time.sleep(60)
             else:
                 raise
-        model_requests.append(ModelRequests(id=uuid.uuid2(), status="bad", image_bytes=config["configurable"]["image_bytes"]))
-        return None
+    print("=" * 20, "Fail", "=" * 20)
+    print(f"Response time = {time.time() - start_time:.2f}")
+    model_requests.append(ModelRequests(id=len(model_requests) + 1, status="bad", image_bytes=config["configurable"]["image_bytes"]))
+    return None
 
 
-print("Safely starting the agent")
-response = safely_exec_agent(extractor_agent, 3)
-if response is not None:
-    print(f"ФИО исполнителя: {response['structured_response'].constructor_name};\
-        ФИО заказчика: {response['structured_response'].customer_name}")
-else:
-    print("Error!")
+# FastAPI
+app = FastAPI()
+
+
+@app.get("/", tags=["Main"], summary="Get init message")
+async def root():
+    return {"message": "Hi user!"}
+
+
+@app.post("/images", tags=["Main"], summary="Send image and get FIO from it.")
+async def get_fio(upload_file: UploadFile):
+    image_in_bytes = await upload_file.read()  # читает содержимое как bytes
+
+    response = safely_exec_agent(extractor_agent, 3, image_in_bytes=image_in_bytes)
+
+    post_response = (
+        {
+            "Status": "OK",
+            "Constructor_name": response["structured_response"].constructor_name,
+            "Customer_name": response["structured_response"].customer_name,
+        }
+        if response is not None
+        else {
+            "Status": "OK",
+            "Constructor_name": None,
+            "Customer_name": None,
+        }
+    )
+
+    return post_response
+
+
+if __name__ == "__main__":
+    print("script running")
+
+    uvicorn.run("main:app", reload=True)
+    # response = safely_exec_agent(extractor_agent, 3)
+    # if response is not None:
+    #     print(f"ФИО исполнителя: {response['structured_response'].constructor_name};\
+    #         ФИО заказчика: {response['structured_response'].customer_name}")
+    # else:
+    #     print("Error!")
+    # print(model_requests)
--- schemas/main_schemas.py
+++ schemas/main_schemas.py
@@ -1,6 +1,15 @@
+from typing import Literal
+
 from pydantic import BaseModel, Field
 
 
 class ResponseFormat(BaseModel):
     constructor_name: str | None = Field(None, description="ФИО исполнителя", min_length=2)
     customer_name: str | None = Field(None, description="ФИО заказчика", min_length=2)
+
+
+# Caching
+class ModelRequests(ResponseFormat):
+    id: int = Field(default=0, description="request_id")
+    status: Literal["good", "bad"]
+    image_bytes: bytes
